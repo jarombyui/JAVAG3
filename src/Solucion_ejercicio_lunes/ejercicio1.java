28d7b8e15ecb389e0ba2464679cc9fb04a899b35
Jarom Campos <mcamposro93@ucvvirtual.edu.pe>
2023-12-28 22:46:54 -0500

    git commit -m "first commit javag3"

---
+++ src/Solucion_ejercicio_lunes/ejercicio1.java
@@ -0,0 +1,20 @@
+package Solucion_ejercicio_lunes;
+
+import java.util.RandomAccess;
+
+public class ejercicio1 {
+
+    public static void main(String[] args){
+
+        float radio = 5.2f;
+        final float PI = 3.14f;
+
+        Float AREA = PI*radio*radio;
+
+        System.out.println("El area es: ");
+        System.out.println(AREA);
+
+
+    }
+
+}
